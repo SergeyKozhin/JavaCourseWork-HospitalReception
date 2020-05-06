a3745153e63ae15708f01a48eb3cb7d99db9b6c2
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-06 23:42:19 +0300

    Placed checkbox filter groups into expansion lists

--- client/src/app/checkbox-filter/checkbox-filter.component.ts
+++ client/src/app/checkbox-filter/checkbox-filter.component.ts
@@ -1,19 +1,27 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { CheckboxItem } from './CheckboxItem';
+import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-checkbox-filter',
   templateUrl: './checkbox-filter.component.html',
   styleUrls: ['./checkbox-filter.component.css']
 })
-export class CheckboxFilterComponent implements OnInit {
+export class CheckboxFilterComponent implements AfterViewInit {
+  @ViewChild('panel') panel: MatExpansionPanel;
   @Input() groupName: string;
   @Input() items: CheckboxItem[];
   @Output() toggle = new EventEmitter<string[]>();
 
-  constructor() { }
+  constructor(
+    private cd: ChangeDetectorRef
+  ) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    if (this.items.find(item => item.checked)) {
+      this.panel.open();
+    }
+    this.cd.detectChanges();
   }
 
   onToggle() {
