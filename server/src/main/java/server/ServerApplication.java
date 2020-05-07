1165b93e82fedd641b80aa2ab36846347a8910a1
Sergey Kozhin <kozhin.sv@edu.spbstu.ru>
2020-05-07 15:46:25 +0300

    Added forwarding for not found to fix routing issues

--- server/src/main/java/server/ServerApplication.java
+++ server/src/main/java/server/ServerApplication.java
@@ -2,12 +2,25 @@ package server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return (request, status, model) -> status == HttpStatus.NOT_FOUND
+                ? new ModelAndView("index.html", Collections.emptyMap(), HttpStatus.OK)
+                : null;
     }
 
 }
