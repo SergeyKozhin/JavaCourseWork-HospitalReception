6619db07f0faaed9a087eb45ef8446216ca3c60c
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-07 22:37:26 +0300

    Added form for diagnosis

---
+++ client/src/app/forms/diagnosis-form/diagnosis-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Diagnosis } from '../../doamain/Diagnosis';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-diagnosis-form',
+  templateUrl: './diagnosis-form.component.html',
+  styleUrls: ['./diagnosis-form.component.css']
+})
+export class DiagnosisFormComponent implements OnInit {
+  form = this.fb.group({
+    name: ['', Validators.required]
+  });
+  type: 'add' | 'update';
+
+  constructor(
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: Diagnosis | undefined
+  ) { }
+
+  ngOnInit(): void {
+    this.form.patchValue(this.data);
+    this.type = (this.data.id === undefined) ? 'add' : 'update';
+  }
+
+  get name() { return this.form.get('name'); }
+}
