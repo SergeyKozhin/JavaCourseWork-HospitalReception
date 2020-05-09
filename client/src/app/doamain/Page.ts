f73db77eb55acec512d2d95a00bf70710af1913d
Sergey Kozhin <kozhin.sv@edu.spbstu.ru>
2020-05-09 16:20:51 +0300

    Added page representation to client

---
+++ client/src/app/doamain/Page.ts
@@ -0,0 +1,16 @@
+import {Sort} from "./Sort";
+import {Pageable} from "./Pageable";
+
+export interface Page<T> {
+  content: T[];
+  pageable: Pageable;
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  number: number;
+  size: number;
+  sort: Sort;
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
