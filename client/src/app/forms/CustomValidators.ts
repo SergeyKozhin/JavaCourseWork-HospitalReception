5005a81a077b2c272db6c7a3cd81a6c090f1c5b6
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-14 20:49:02 +0300

    Fixed uniqueness check

--- client/src/app/forms/CustomValidators.ts
+++ client/src/app/forms/CustomValidators.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export function uniqueValidator(data: { name: string }[]): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
-    const notUnique = data?.find(el => el.name === control.value) !== undefined;
+    const notUnique = data?.find(el => el.name.toLowerCase() === control.value.toLowerCase()) !== undefined;
     return notUnique ? { notUnique: { value: control.value } } : null;
   };
 }
