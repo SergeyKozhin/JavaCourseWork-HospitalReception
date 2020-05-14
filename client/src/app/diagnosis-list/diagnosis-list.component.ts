a680cd92b930be642b238ef5aa956c9e6a01846c
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-14 20:59:15 +0300

    Improved diagnosis addition

--- client/src/app/diagnosis-list/diagnosis-list.component.ts
+++ client/src/app/diagnosis-list/diagnosis-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Diagnosis } from '../doamain/Diagnosis';
 import { DiagnosisService } from '../services/diagnosis.service';
 import { MatDialog } from '@angular/material/dialog';
@@ -12,7 +12,6 @@ import { ConfirmationWindowComponent } from '../forms/confirmation-window/confir
   styleUrls: ['./diagnosis-list.component.css']
 })
 export class DiagnosisListComponent implements OnInit {
-  @ViewChild('list') listDiv: ElementRef;
   diagnoses: Diagnosis[];
 
   constructor(
@@ -42,9 +41,11 @@ export class DiagnosisListComponent implements OnInit {
             .subscribe(newDiagnosis => {
                 this.snackbarService.showInfoSnackbar('Diagnosis successfully added.');
                 this.diagnoses.push(newDiagnosis);
-                requestAnimationFrame(() =>
-                  this.listDiv.nativeElement.scrollTop = this.listDiv.nativeElement.scrollHeight
-                );
+                this.diagnoses = this.diagnoses.sort((a, b) => a.name.localeCompare(b.name));
+                requestAnimationFrame(() => {
+                  const item = document.getElementById(newDiagnosis.id.toString());
+                  item.scrollIntoView(true);
+                });
               },
               error => this.snackbarService.showErrorSnackbar(`Couldn't add diagnosis: ${error}`));
         }
