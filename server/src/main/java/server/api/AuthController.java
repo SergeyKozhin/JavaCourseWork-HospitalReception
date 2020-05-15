9492fc7e3a02011796251fc5504a853cce3c7cd1
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-15 21:14:29 +0300

    Refactored auth controller with refresh token

--- server/src/main/java/server/api/AuthController.java
+++ server/src/main/java/server/api/AuthController.java
@@ -2,62 +2,52 @@ package server.api;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import server.domain.AuthRequest;
-import server.domain.User;
-import server.security.jwt.JwtTokenProvider;
+import server.security.AccountService;
 
-import java.util.HashMap;
+import javax.validation.Valid;
 import java.util.Map;
+import java.util.Optional;
 
-@Log4j2
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin("*")
 public class AuthController {
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserDetailsService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
+
+    private final AccountService accountService;
 
     @Autowired
-    public AuthController(
-            JwtTokenProvider jwtTokenProvider,
-            @Qualifier("customUserDetailService") UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.userDetailsService = userDetailsService;
-        this.passwordEncoder = passwordEncoder;
+    public AuthController(AccountService accountService) {
+        this.accountService = accountService;
+
     }
 
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody AuthRequest request) {
-        try {
-            String username = request.getUsername();
-            User user = (User) userDetailsService.loadUserByUsername(username);
+    public Map<String, Object> login(@RequestBody @Valid AuthRequest request) {
+        return accountService.loginUser(request.getUsername(), request.getPassword());
+    }
 
-            if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-                throw new BadCredentialsException("Invalid password");
-            }
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refreshToken(@RequestParam String refreshToken) {
+        return accountService.refreshToken(refreshToken).map(newToken ->
+                ResponseEntity.ok(Map.of("jwtToken", newToken))).orElse(
+                new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED)
+        );
+    }
 
-            String token = jwtTokenProvider.createToken(username, user.getRoles());
-
-            Map<String, Object> model = new HashMap<>();
-            model.put("username", username);
-            model.put("roles", user.getRoles());
-            model.put("token", token);
-
-            return model;
-        } catch (AuthenticationException e) {
-            throw new BadCredentialsException("Invalid username or password");
-        }
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(@RequestParam String refreshToken) {
+        accountService.logoutUser(refreshToken);
     }
 }
--- server/src/main/java/server/domain/AuthRequest.java
+++ server/src/main/java/server/domain/AuthRequest.java
@@ -3,11 +3,16 @@ package server.domain;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
 @RequiredArgsConstructor
 public class AuthRequest implements Serializable {
+
+    @NotBlank(message = "Username is required")
     private final String username;
+
+    @NotBlank(message = "Password is required")
     private final String password;
 }
