547e7e803e771bfd4e98d20df8cd712fbf730c16
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-15 18:43:18 +0300

    Updated authentication on server

--- server/src/main/java/server/api/AuthController.java
+++ server/src/main/java/server/api/AuthController.java
@@ -1,57 +1,61 @@
 package server.api;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import server.data.UserRepository;
 import server.domain.AuthRequest;
+import server.domain.User;
 import server.security.jwt.JwtTokenProvider;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+@Log4j2
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin("*")
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepo;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserRepository userRepo) {
-        this.authenticationManager = authenticationManager;
+    public AuthController(
+            JwtTokenProvider jwtTokenProvider,
+            @Qualifier("customUserDetailService") UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
         this.jwtTokenProvider = jwtTokenProvider;
-        this.userRepo = userRepo;
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
+    public Map<String, Object> login(@RequestBody AuthRequest request) {
         try {
-            String name = request.getUsername();
-            List<String> roles = userRepo.findUserByUsername(name)
-                    .orElseThrow(() -> new UsernameNotFoundException("Username not found: " + name))
-                    .getRoles();
-            String token = jwtTokenProvider.createToken(
-                    name,
-                    roles
-            );
+            String username = request.getUsername();
+            User user = (User) userDetailsService.loadUserByUsername(username);
 
-            Map<Object, Object> model = new HashMap<>();
-            model.put("username", name);
-            model.put("roles", roles);
+            if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+                throw new BadCredentialsException("Invalid password");
+            }
+
+            String token = jwtTokenProvider.createToken(username, user.getRoles());
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("username", username);
+            model.put("roles", user.getRoles());
             model.put("token", token);
 
-            return ResponseEntity.ok(model);
+            return model;
         } catch (AuthenticationException e) {
             throw new BadCredentialsException("Invalid username or password");
         }
