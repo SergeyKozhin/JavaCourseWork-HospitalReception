a8c801c1485f6a49c9f404192543b3008d803bba
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-15 23:35:59 +0300

    Added functionality to loin page

--- client/src/app/login-from/login-form.component.ts
+++ client/src/app/login-from/login-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-from',
@@ -11,13 +13,23 @@ export class LoginFormComponent {
     username: ['', Validators.required],
     password: ['', Validators.required]
   });
+  failed = false;
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
   ) { }
 
   onSubmit() {
-
+    this.authService.login(this.form.value)
+      .subscribe(success => {
+        if (success) {
+          this.router.navigate(['']).finally();
+        } else {
+          this.failed = true;
+        }
+      });
   }
 
   get username() { return this.form.get('username'); }
