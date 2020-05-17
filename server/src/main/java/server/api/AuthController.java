f020f02928d24e1d94a643f8ea5627525ae12443
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-17 18:10:54 +0300

    Added XSRF prefetch

--- server/src/main/java/server/api/AuthController.java
+++ server/src/main/java/server/api/AuthController.java
@@ -1,26 +1,21 @@
 package server.api;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import server.domain.AuthRequest;
 import server.security.AccountService;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
---
+++ server/src/main/java/server/api/ConfigController.java
@@ -0,0 +1,16 @@
+package server.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/config")
+public class ConfigController {
+    @GetMapping
+    public ResponseEntity<?> getConfig() {
+        return  new ResponseEntity<>(HttpStatus.OK);
+    }
+}
