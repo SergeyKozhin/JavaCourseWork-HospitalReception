b47cf39d86fffc10aa6dcd793536fa92aef1786a
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-17 18:33:28 +0300

    Added buttons to add new diagnosis and ward in patient form

--- client/src/app/forms/patient-form/patient-form.component.ts
+++ client/src/app/forms/patient-form/patient-form.component.ts
@@ -1,10 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { DiagnosisService } from '../../services/diagnosis.service';
 import { WardService } from '../../services/ward.service';
 import { Diagnosis } from '../../doamain/Diagnosis';
 import { Ward } from '../../doamain/Ward';
+import { DiagnosisFormComponent } from '../diagnosis-form/diagnosis-form.component';
+import { SnackbarService } from '../../services/snackbar.service';
+import { WardFormComponent } from '../ward-form/ward-form.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-patient-form',
@@ -30,6 +34,9 @@ export class PatientFormComponent implements OnInit {
     private fb: FormBuilder,
     private diagnosisService: DiagnosisService,
     private wardService: WardService,
+    private snackbarService: SnackbarService,
+    public dialog: MatDialog,
+    public authService: AuthService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
@@ -87,5 +94,45 @@ export class PatientFormComponent implements OnInit {
 
   get ward() {
     return this.form.get('ward');
+  }
+
+  addDiagnosis() {
+    const dialogRef = this.dialog.open(DiagnosisFormComponent, {
+      data: {} as Diagnosis
+    });
+    dialogRef.componentInstance.diagnoses = this.diagnoses;
+    dialogRef.afterClosed()
+      .subscribe(data => {
+        if (data) {
+          this.diagnosisService.addDiagnosis(data)
+            .subscribe(newDiagnosis => {
+                this.snackbarService.showInfoSnackbar('Diagnosis successfully added.');
+                this.diagnoses.push(newDiagnosis);
+                this.diagnoses = this.diagnoses.sort((a, b) => a.name.localeCompare(b.name));
+                this.diagnosis.setValue(newDiagnosis);
+              },
+              error => this.snackbarService.showErrorSnackbar(`Couldn't add diagnosis: ${error}`));
+        }
+      });
+  }
+
+  addWard() {
+    const dialogRef = this.dialog.open(WardFormComponent, {
+      data: {} as Ward
+    });
+    dialogRef.componentInstance.wards = this.wards;
+    dialogRef.afterClosed()
+      .subscribe(data => {
+        if (data) {
+          this.wardService.addWard(data)
+            .subscribe(newWard => {
+                this.snackbarService.showInfoSnackbar('Ward successfully added.');
+                this.wards.push(newWard);
+                this.wards = this.wards.sort((a, b) => a.name.localeCompare(b.name));
+                this.ward.setValue(newWard);
+              },
+              error => this.snackbarService.showErrorSnackbar(`Couldn't add ward: ${error}`));
+        }
+      });
   }
 }
