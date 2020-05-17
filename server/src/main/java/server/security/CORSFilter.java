cd50e8487b5050e9d4efe0c2f3fe7379f5ae635b
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-05-17 18:21:58 +0300

    Allowed CORS from any domain

--- server/src/main/java/server/security/CORSFilter.java
+++ server/src/main/java/server/security/CORSFilter.java
@@ -21,9 +21,10 @@ public class CORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE, HEAD");
         response.setHeader("Access-Control-Max-Age", "3600");
